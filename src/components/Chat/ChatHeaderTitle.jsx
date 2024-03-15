ea59ff45e034216e64b4b7ed47bc6136010be74e
Shahan-nub <shahananwer786@gmail.com>
2024-03-15 20:39:34 +0530

    menu size

--- src/components/Chat/ChatHeaderTitle.jsx
+++ src/components/Chat/ChatHeaderTitle.jsx
@@ -10,7 +10,7 @@ export default function ChatHeaderTitle() {
   return (
     <>
     <div className="flex text-slate-200  items-center text-nowrap basis-[40%] md:ml-2 text-lg md:text-2xl font-semibold font-sans lg:pl-0 pl-10">
-      <MdMenu className={`mr-4 lg:hidden`}
+      <MdMenu className={`mr-4 text-xl lg:hidden`}
       onClick={() => {dispatch(setMenu());
       console.log(menuState)
       }}
